9b1a3c827d5da7659eb5cae4a1fb4d3f6cd0a638
Sujay Anjankar <sanjankar@infocepts.com>
2016-01-14 14:01:52 +0530

    Refactoring.

---
+++ src/com/had/handlers/HelloHandler.java
@@ -0,0 +1,16 @@
+package com.had.handlers;
+
+/**
+ * @author Shubham S Patil
+ * @since January 13, 2016
+ */
+public class HelloHandler {
+
+	public HelloHandler() {
+	}
+
+	public String getHelloString() {
+		return "Hello!";
+	}
+
+}
