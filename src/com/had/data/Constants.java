d59a4749d1f5926cc300f2304a5c4117badfdbdf
Shubham Patil <shubhamdraco.p24@gmail.com>
2016-01-17 19:46:19 +0530

    Added Statewise Population Chart & Modified Project Constants to support deployment to OpenShift Changed Files :    * Constants.java    * index.html

--- src/com/had/data/Constants.java
+++ src/com/had/data/Constants.java
@@ -8,8 +8,22 @@ package com.had.data;
 public class Constants {
     // JDBC driver name and database URL
     public static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-    public static final String DB_URL = "jdbc:mysql://localhost/had";
+    public static String DB_URL;
     //  Database credentials
-    public static final String USER = "root";
-    public static final String PASS = "mysqlroot";
+    public static String USER;
+    public static String PASS;
+    static {
+        if(System.getenv("OPENSHIFT_MYSQL_DB_HOST")!=null)
+        {
+            USER = System.getenv("OPENSHIFT_MYSQL_DB_USERNAME");
+            PASS = System.getenv("OPENSHIFT_MYSQL_DB_PASSWORD");
+            DB_URL = "jdbc:mysql://"+System.getenv("OPENSHIFT_MYSQL_DB_HOST")+":"+System.getenv("$OPENSHIFT_MYSQL_DB_PORT")+"/jbossews";
+        }
+        else
+        {
+            DB_URL = "jdbc:mysql://localhost/had";
+            USER = "root";
+            PASS = "mysqlroot";
+        }
+    }
 }
