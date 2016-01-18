dc6407843dbf1940eaf8822c5ef2676476b2bd1e
Shubham Patil <shubhamdraco.p24@gmail.com>
2016-01-18 11:20:09 +0530

    Fix LOAD_ALL_POPULATION_QUERY to include "," in between column names in place of " "

--- src/com/had/handlers/PopulationHandler.java
+++ src/com/had/handlers/PopulationHandler.java
@@ -18,8 +18,8 @@ public class PopulationHandler {
 	private static String STATE_COLUMN_NAME = "STATE";
 	private static String POP_2001_COLUMN_NAME = "POP_2001";
 	private static String POP_2011_COLUMN_NAME = "POP_2011";
-	private static String LOAD_ALL_POPULATION_QUERY = "SELECT " + STATE_COLUMN_NAME + " " + POP_2001_COLUMN_NAME
-			+ " " + POP_2011_COLUMN_NAME + " FROM POPULATION";
+	private static String LOAD_ALL_POPULATION_QUERY = "SELECT " + STATE_COLUMN_NAME + "," + POP_2001_COLUMN_NAME
+			+ "," + POP_2011_COLUMN_NAME + " FROM POPULATION";
 	public PopulationHandler() {
 	}
 
@@ -30,7 +30,7 @@ public class PopulationHandler {
 			Class.forName(Constants.JDBC_DRIVER);
 			try (Connection connection = DriverManager.getConnection(Constants.DB_URL, Constants.USER, Constants.PASS);
 					Statement statement = connection.createStatement();
-					ResultSet rs = statement.executeQuery(LOAD_ALL_POPULATION_QUERY);) {
+					ResultSet rs = statement.executeQuery(LOAD_ALL_POPULATION_QUERY)) {
 				while (rs.next()) {
 					populationDAO.addPopulation(
 							new Population(rs.getString(STATE_COLUMN_NAME), rs.getInt(POP_2001_COLUMN_NAME), rs.getInt(POP_2011_COLUMN_NAME)));
