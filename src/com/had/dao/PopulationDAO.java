5ba8c3a2fb023c17ccc8f866eb1961f21a1fc1e5
Shubham Patil <shubhamdraco.p24@gmail.com>
2016-01-18 21:20:49 +0530

    Move DB Interaction logic to DAO & Fix redundant JARs Changed files:    * PopulationDAO.java    * PopulationHandler.java

--- src/com/had/dao/PopulationDAO.java
+++ src/com/had/dao/PopulationDAO.java
@@ -1,18 +1,45 @@
 package com.had.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+
+import com.google.gson.Gson;
 import com.had.data.Population;
+import com.had.util.Constants;
 
 /**
  * @author Shubham S Patil
  * @since January 13, 2016
  */
 public class PopulationDAO {
-
+    private static String STATE_COLUMN_NAME = "STATE";
+    private static String POP_2001_COLUMN_NAME = "POP_2001";
+    private static String POP_2011_COLUMN_NAME = "POP_2011";
+    private static String LOAD_ALL_POPULATION_QUERY = "SELECT " + STATE_COLUMN_NAME + "," + POP_2001_COLUMN_NAME
+            + "," + POP_2011_COLUMN_NAME + " FROM POPULATION";
     List<Population> populations;
 	public PopulationDAO() {
         populations = new ArrayList<>();
+        try
+        {
+            Class.forName(Constants.JDBC_DRIVER);
+            try (Connection connection = DriverManager.getConnection(Constants.DB_URL, Constants.USER, Constants.PASS);
+                 Statement statement = connection.createStatement();
+                 ResultSet rs = statement.executeQuery(LOAD_ALL_POPULATION_QUERY)) {
+                while (rs.next()) {
+                    addPopulation(
+                            new Population(rs.getString(STATE_COLUMN_NAME), rs.getInt(POP_2001_COLUMN_NAME), rs.getInt(POP_2011_COLUMN_NAME)));
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
 	}
     public void addPopulation(Population obj)
     {
--- src/com/had/handlers/PopulationHandler.java
+++ src/com/had/handlers/PopulationHandler.java
@@ -15,34 +15,12 @@ import java.sql.Statement;
  * @since January 13, 2016
  */
 public class PopulationHandler {
-	private static String STATE_COLUMN_NAME = "STATE";
-	private static String POP_2001_COLUMN_NAME = "POP_2001";
-	private static String POP_2011_COLUMN_NAME = "POP_2011";
-	private static String LOAD_ALL_POPULATION_QUERY = "SELECT " + STATE_COLUMN_NAME + "," + POP_2001_COLUMN_NAME
-			+ "," + POP_2011_COLUMN_NAME + " FROM POPULATION";
 	public PopulationHandler() {
 	}
 
 	public String getPopulationData() {
 		PopulationDAO populationDAO = new PopulationDAO();
-		try
-		{
-			Class.forName(Constants.JDBC_DRIVER);
-			try (Connection connection = DriverManager.getConnection(Constants.DB_URL, Constants.USER, Constants.PASS);
-					Statement statement = connection.createStatement();
-					ResultSet rs = statement.executeQuery(LOAD_ALL_POPULATION_QUERY)) {
-				while (rs.next()) {
-					populationDAO.addPopulation(
-							new Population(rs.getString(STATE_COLUMN_NAME), rs.getInt(POP_2001_COLUMN_NAME), rs.getInt(POP_2011_COLUMN_NAME)));
-				}
-			}
-            return new Gson().toJson(populationDAO.getPopulations());
-		}
-		catch (Exception e)
-		{
-            e.printStackTrace();
-			return new Gson().toJson("Unhandled Exception");
-		}
+		return new Gson().toJson(populationDAO.getPopulations());
     }
 
 }
