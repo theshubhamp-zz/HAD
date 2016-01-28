1b05bdcb46cb6610fd89532e68fa9152c6af2fd3
Shubham Patil <shubhamdraco.p24@gmail.com>
2016-01-28 17:47:46 +0530

    Add GROUP BY capability to Dynamic Query Builder Changed Files:    * QueryHelper.java

--- src/com/had/util/QueryHelper.java
+++ src/com/had/util/QueryHelper.java
@@ -1,7 +1,6 @@
 package com.had.util;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -9,43 +8,39 @@ import java.util.Map;
  * @since January 23, 2016
  */
 public class QueryHelper {
-    public static String selectQuery(ArrayList<String> columns, HashMap<String,String> tables, HashMap<String,String> joins)
-    {
+    public static String selectQuery(List<String> columns, Map<String, String> tables, Map<String, String> joins, String groupby) {
         String query = "";
-        query+="SELECT ";
-        if (columns.size()==0)
-        {
-            query+= "* ";
-        }
-        else
-        {
+        query += "SELECT ";
+        if (columns.size() == 0) {
+            query += "* ";
+        } else {
             for (int i = 0; i < columns.size(); i++) {
-                query+= columns.get(i);
-                if(i<columns.size()-1)
-                {
-                    query+= ",";
+                query += columns.get(i);
+                if (i < columns.size() - 1) {
+                    query += ", ";
                 }
             }
         }
-        query+=" FROM ";
-        int i=0;
-        for (Map.Entry<String,String> entry: tables.entrySet()) {
-            query+= " " + entry.getKey() + " AS " + entry.getValue() + " ";
-            if(i<tables.size()-1)
-                query+=",";
+        query += " FROM ";
+        int i = 0;
+        for (Map.Entry<String, String> entry : tables.entrySet()) {
+            query += entry.getKey() + " AS " + entry.getValue() + " ";
+            if (i < tables.size() - 1)
+                query += ", ";
             i++;
         }
-        i=0;
-        if (!(joins.size()==0))
-        {
-            query+=" WHERE ";
-            for (Map.Entry<String,String> entry: joins.entrySet()) {
-                query+=" "+entry.getKey()+" = "+entry.getValue()+" ";
-                if (i<joins.size()-1)
-                    query+=" AND ";
+        i = 0;
+        if (!(joins.size() == 0)) {
+            query += " WHERE ";
+            for (Map.Entry<String, String> entry : joins.entrySet()) {
+                query += " " + entry.getKey() + " = " + entry.getValue() + " ";
+                if (i < joins.size() - 1)
+                    query += " AND ";
                 i++;
             }
         }
+        if (groupby != null)
+            query += " GROUP BY " + groupby;
         return query;
     }
 }
