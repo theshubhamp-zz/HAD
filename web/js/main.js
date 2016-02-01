00a51127d5a0688656b94b5adf5eacbca79fdbde
Shubham Patil <shubhamdraco.p24@gmail.com>
2016-02-01 14:36:29 +0530

    Changed chart workflow and Added graph for Per Capita Income Changed Files:    * main.js

--- web/js/main.js
+++ web/js/main.js
@@ -1,20 +1,18 @@
 var cidx =0;
 $(document).ready(function(){
-    $("#popchart").show();
-    $("#gdpchart").hide();
-    $("#litchart").hide();
+    $("#chart").show();
 });
 $('#toggle').click(function(){
     cidx++;
     $('#chart').html('');
-    cidx = cidx%3;
+    cidx = cidx%4;
     if(cidx==0)
     {
         $('#chart').html('<h2>Population Statewise for year 2011</h2>>');
-        var svg = dimple.newSvg("#chart", 1200, 600);
+        var svg = dimple.newSvg("#chart", 1360, 600);
         d3.json("http://"+location.hostname+"/api/population", function (data) {
             var myChart = new dimple.chart(svg, data);
-            //myChart.setBounds(60, 60, 1200, 700);
+            myChart.setBounds(60, 60, 1300, 400);
             var x = myChart.addCategoryAxis("x", "name");
             var y = myChart.addMeasureAxis("y", "populationYear2011");
             myChart.addSeries(null, dimple.plot.bar);
@@ -26,10 +24,10 @@ $('#toggle').click(function(){
     else if(cidx==1)
     {
         $('#chart').html('<h2>GDP Statewise for year 2011</h2>>');
-        var svg = dimple.newSvg("#chart", 1200, 600);
+        var svg = dimple.newSvg("#chart", 1360, 600);
         d3.json("http://"+location.hostname+"/api/gdp", function (data) {
             var myChart = new dimple.chart(svg, data);
-            //myChart.setBounds(60, 60, 1200, 700);
+            myChart.setBounds(60, 60, 1300, 400);
             var x = myChart.addCategoryAxis("x", "name");
             var y = myChart.addMeasureAxis("y", "gdpYear2010_11");
             myChart.addSeries(null, dimple.plot.bar);
@@ -38,18 +36,32 @@ $('#toggle').click(function(){
             y.titleShape.text("GDP in 2010-11");
         });
     }
-    else{
+    else if(cidx==2){
         $('#chart').html('<h2>Literacy Rate Statewise for year 2011</h2>>');
-        var svg = dimple.newSvg("#chart", 1200, 600);
+        var svg = dimple.newSvg("#chart", 1360, 600);
         d3.json("http://"+location.hostname+"/api/literacy", function (data) {
             var myChart = new dimple.chart(svg, data);
-            //myChart.setBounds(60, 60, 1200, 700);
+            myChart.setBounds(60, 60, 1300, 400);
             var x = myChart.addCategoryAxis("x", "name");
             var y = myChart.addMeasureAxis("y", "literacyRate2011");
             myChart.addSeries(null, dimple.plot.bar);
             myChart.draw();
             x.titleShape.text("States");
             y.titleShape.text("Literacy in 2011");
+        });
+    }
+    else{
+        $('#chart').html('<h2>Per Capita Income Statewise for year 2011</h2>>');
+        var svg = dimple.newSvg("#chart", 1360, 600);
+        d3.json("http://"+location.hostname+"/api/pci", function (data) {
+            var myChart = new dimple.chart(svg, data);
+            myChart.setBounds(60, 60, 1300, 400);
+            var x = myChart.addCategoryAxis("x", "stateName");
+            var y = myChart.addMeasureAxis("y", "percapitaYear2010_11");
+            myChart.addSeries(null, dimple.plot.bar);
+            myChart.draw();
+            x.titleShape.text("States");
+            y.titleShape.text("PCI in 2011");
         });
     }
 });
