1126feb775296698a158734ee00b736b4aa514db
Shubham Patil <shubhamdraco.p24@gmail.com>
2016-02-09 16:16:08 +0530

    Final Commit before demo

--- web/js/export.js
+++ web/js/export.js
@@ -1,8 +1,8 @@
-function getPdf(event) {
-	html2canvas(event.currentTarget, {
-		onrendered : function (canvas) {
+function exp(){
+	html2canvas(document.getElementById('export'), {
+		onrendered: function (canvas) {
 			var imgData = canvas.toDataURL("image/png", 1.0);
-			var pdf = new jsPDF();
+			var pdf = new jsPDF('landscape');
 			pdf.addImage(imgData, 'PNG', 0, 0);
 			pdf.save("download.pdf");
 		}
